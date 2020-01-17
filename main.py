7e6b790cbaf3dba0243d76d42d571c4fc152838d
Utkarsh Bhardwaj <utbh23041999@gmail.com>
2020-01-17 17:11:49 +0530

    Final implementation for contour

--- main.py
+++ main.py
@@ -4,17 +4,29 @@ import cv2
 import numpy as np
 
 
-def toSkeleton(image):
-    skeleton = np.zeros(image.shape, np.uint8)
+def toSkeleton(image, erosion):
+    # Params:
+    thresh_val = 127
+    max_val = 255
+    open_iter = 10
+    close_iter = 3
+
+
     size = np.size(image)
 
-    _, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    _, image = cv2.threshold(image, thresh_val, max_val, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (erosion, erosion))
     # done = False
 
-    skeleton = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel, iterations=20)
-    skeleton = cv2.morphologyEx(skeleton, cv2.MORPH_CLOSE, kernel, iterations=20)
+    skeleton = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations=close_iter)
+    skeleton = cv2.morphologyEx(skeleton, cv2.MORPH_OPEN, kernel, iterations=open_iter)
+
+    # skeleton = image
+
+    # skeleton = cv2.erode(skeleton, kernel)
+    # skeleton = cv2.dilate(skeleton, kernel)
+    # skeleton = image
 
     # while not done:
     #     eroded = cv2.erode(image, kernel)
@@ -37,34 +49,22 @@ def toSkeleton(image):
     return skeleton
 
 
-def toDefog(image):
+def toChannels(image):
+    channels = cv2.split(image)
+    for channel in channels:
+        channel_img = cv2.bilateralFilter(channel, 20, 120, 1)
+        # channel_img = cv2.GaussianBlur(channel, (3, 3), 0)
 
-    sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-    sharpen = cv2.filter2D(image, -1, sharpen_kernel)
+        if debug == 1:
+            cv2.namedWindow("Channel image", cv2.WINDOW_KEEPRATIO)
+            cv2.imshow("Channel image", channel_img)
+            x = cv2.waitKey(0)
+            if x == 27:
+                cv2.destroyWindow('Channel image')
 
-    if debug==1:
-        cv2.namedWindow("Defogged image", cv2.WINDOW_KEEPRATIO)
-        cv2.imshow("Defogged image", sharpen)
-        x = cv2.waitKey(0)
-        if x == 27:
-            cv2.destroyWindow('Defogged image')
+    combined_channels = cv2.bitwise_and(*channels)
 
-    return sharpen
-
-
-def toGrayscale(image):
-    blurred = cv2.bilateralFilter(image, 20, 120, 1)
-    # blurred = cv2.GaussianBlur(image, (3, 3), 0)
-    gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-
-    if debug==1:
-        cv2.namedWindow("Grayscale image", cv2.WINDOW_KEEPRATIO)
-        cv2.imshow("Grayscale image", gray)
-        x = cv2.waitKey(0)
-        if x == 27:
-            cv2.destroyWindow('Grayscale image')
-
-    return gray
+    return combined_channels
 
 
 def toCanny(image, sigma):
@@ -82,6 +82,24 @@ def toCanny(image, sigma):
             cv2.destroyWindow('Canny Edge Detection')
 
     return canny_edges
+
+
+def toContour(image, edges):
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    if debug == 1:
+        for i in range(len(contours)):
+            cv2.namedWindow("Contour Lines", cv2.WINDOW_NORMAL)
+
+            lines_image = np.zeros(image.shape, np.uint8)
+            cv2.drawContours(lines_image, contours, i, (0, 255, 0), 2)
+
+            cv2.imshow("Contour Lines", lines_image)
+            x = cv2.waitKey(0)
+            if x == 27:
+                cv2.destroyWindow('Contour Lines')
+
+    return contours
 
 
 def toHoughImage(image, edges):
@@ -121,11 +139,11 @@ def genHoughLines(edges):
     return lines
 
 
-def createMask(image):
-    grayscale_image = toGrayscale(img)
-    skeleton_image = toSkeleton(grayscale_image)
-    # defog_image = toDefog(grayscale_image)
+def createMask(image, args):
+    combined_image = toChannels(img)
+    skeleton_image = toSkeleton(combined_image, args.erosion)
     canny_edges = toCanny(skeleton_image, args.sigma)
+    draw_contours = toContour(img, canny_edges)
     hough_lines = toHoughImage(img, canny_edges)
 
     return hough_lines
@@ -135,6 +153,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create sky mask for landscape')
     parser.add_argument('origin', help='Source of image file (JPG/PNG/TIFF/BMP supported)')
     parser.add_argument('-s', '--sigma', help='Sigma for Canny detection', type=float, default=0.33)
+    parser.add_argument('-e', '--erosion', help='Erosion factor for Canny preparation', type=int, default=3)
     parser.add_argument('-d', '--destination', help='Destination of image file (JPG/PNG/TIFF/BMP supported)')
     parser.add_argument('-v', '--debug', help='Debugging for intermediate verbosity', default=0, action='store_true')
     args = parser.parse_args()
@@ -150,7 +169,7 @@ if __name__ == "__main__":
     cv2.namedWindow("original", cv2.WINDOW_NORMAL)
     cv2.imshow("original", img)
 
-    mask = createMask(img)
+    mask = createMask(img, args)
 
     if not args.destination:
         out_path = '{}_mask.png'.format(args.origin[args.origin.rfind('/')+1:args.origin.find('.')])
